42a9a138e53f13cb171075b1ac2d14e8030632c7
Baranai Tudor <73432645+tudorbaranai@users.noreply.github.com>
2026-05-05 08:44:46 +0300

    Adaugă metode pentru Student în interfața IStocareData

--- NivelStocareDate/IStocareData.cs
+++ NivelStocareDate/IStocareData.cs
@@ -10,6 +10,10 @@ namespace NivelStocareDate
         void AddChirias(Chirias ch);
         List<Chirias> GetChiriasi();
 
+        void AddStudent(Student st);
+        List<Student> GetStudenti();
+        Student CautaStudentDupaId(int id);
+
         Apartament CautaDupaNumar(int numar);
         List<Apartament> CautaDupaEtaj(int etaj);
         List<Apartament> CautaDupaPretMaxim(double pretMax);
